aa62aa0e1e99660568caa307ab260aacad04269c
Nawaz <k.nawaz.h@gmail.com>
2019-01-28 21:23:42 -0500

    v2 largest common prefix

--- JavaSolutions/LongestCommonPrefix.java
+++ JavaSolutions/LongestCommonPrefix.java
@@ -2,26 +2,13 @@ class Solution {
     public String longestCommonPrefix(String[] strs) {
         if(strs.length == 0)
             return "";
-        Map<String, Integer> myMap = new HashMap<String, Integer>();
-        for(int i = 0; i < strs.length; i++){
-            for(int j = 0; j < strs[i].length(); j++){
-                if(myMap.containsKey(strs[i].substring(0,j+1))){
-                    int count = myMap.get(strs[i].substring(0,j+1));
-                    count++;
-                    myMap.put(strs[i].substring(0,j+1), count);
-                }
-                else{
-                    myMap.put(strs[i].substring(0,j+1), 1);
-                }
+        String prefix = strs[0];
+        for(int i = 1; i < strs.length; i++){
+            while(strs[i].indexOf(prefix) != 0){
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if(prefix.length() == 0) return "";
             }
         }
-        int maxLen = strs.length;
-        String result = "";
-        for(String key : myMap.keySet()){
-            if(myMap.get(key) >= maxLen && key.length() >= result.length()){
-                result = key;
-            }
-        }
-        return result;
+        return prefix;
     }
 }
