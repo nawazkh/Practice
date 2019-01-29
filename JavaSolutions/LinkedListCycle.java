c661126a8d9b1679091c664db5ce1bcd9cfca6f7
Nawaz <k.nawaz.h@gmail.com>
2019-01-29 13:28:06 -0500

    LL cycle java

---
+++ JavaSolutions/LinkedListCycle.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+       Set<ListNode> mySet = new HashSet<ListNode>();
+        while(head != null){
+            if(mySet.contains(head)) return true;
+            mySet.add(head);
+            head = head.next;
+        }
+        return false;
+    }
+}
