a4f7cecbe50f0be14785608b5eaf164818418e26
Nawaz <k.nawaz.h@gmail.com>
2019-02-02 15:57:33 -0500

    implement data structure of our own

---
+++ JavaSolutions/MinStack.java
@@ -0,0 +1,49 @@
+class MinStack {
+    private Node head;
+    /** initialize your data structure here. */
+    public MinStack() {
+        this. head = null;
+    }
+    
+    public void push(int x) {
+        if(head == null)
+            head = new Node(x,x);
+        else
+            head = new Node(x,Math.min(head.min,x),head);
+    }
+    
+    public void pop() {
+        head = head.next;
+    }
+    
+    public int top() {
+        return head.val;
+    }
+    
+    public int getMin() {
+        return head.min;
+    }
+    class Node{
+        int val;
+        int min;
+        Node next;
+        
+        Node(int val, int min){
+            this(val, min, null);
+        }
+        Node(int val, int min, Node n){
+            this.val = val;
+            this.min = min;
+            this.next = n;
+        }
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
