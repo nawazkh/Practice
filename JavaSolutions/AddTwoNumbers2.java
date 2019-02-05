5b69c65dd2669f99e5ec258d55b39c3da26bcb0b
Nawaz <k.nawaz.h@gmail.com>
2019-02-05 14:59:09 -0500

    add two numbers 2 Linked Lists

---
+++ JavaSolutions/AddTwoNumbers2.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if(l1 == null && l2 == null) return null;
+        int carry = 0;
+        ListNode head = new ListNode(0);
+        head.next = null;
+        ListNode s = head;
+        l1 = reverse(l1);
+        l2 = reverse(l2);
+        while(true){
+            if(l1 != null && l2 != null){
+                int temp = l1.val + l2.val + carry;
+                carry = (temp >= 10) ? 1 : 0;
+                head.next = new ListNode(temp%10);
+                head = head.next;
+                l1 = l1.next;
+                l2 = l2.next;
+            }else if(l1 == null && l2 != null){
+                int temp = l2.val + carry;
+                carry = (temp >= 10) ? 1 : 0;
+                head.next = new ListNode(temp%10);
+                head = head.next;
+                l2 = l2.next;
+            }else if(l2 == null && l1 != null){
+                //copy all l1
+                int temp = l1.val + carry;
+                carry = (temp >= 10) ? 1 : 0;
+                head.next = new ListNode(temp%10);
+                head = head.next;
+                l1 = l1.next;
+            }else{
+                if(carry == 1){
+                    head.next = new ListNode(1);
+                    head = head.next;
+                }
+                head.next = null;
+                break;
+            }
+        }
+        return reverse(s.next);
+    }
+    public ListNode reverse(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = null;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
