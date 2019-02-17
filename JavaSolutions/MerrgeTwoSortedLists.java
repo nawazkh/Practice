85e1557e1212b6ee86980cbaf4aea841de137d38
Nawaz <k.nawaz.h@gmail.com>
2019-02-17 16:04:54 -0500

    merge two sorted lists rercursive and iteraative

--- JavaSolutions/MerrgeTwoSortedLists.java
+++ JavaSolutions/MerrgeTwoSortedLists.java
@@ -8,30 +8,33 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1 == null && l2 == null) return null;
+        if(l1 == null) return l2;
+        else if(l2 == null) return l1;
+        if(l1.val < l2.val){
+            l1.next = mergeTwoLists(l1.next,l2);
+            return l1;
+        }else{
+            l2.next = mergeTwoLists(l1,l2.next);
+            return l2;
+        }
+    }
+}
+
+class Solution{
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(0);
-        ListNode start = dummy;
-        while(true){
-            if(l1 == null){
-                dummy.next = l2;
-                break;
-            }
-            if(l2 == null){
-                dummy.next = l1;
-                break;
-            }
+        ListNode temp = dummy;
+        while(l1 != null && l2 != null){
             if(l1.val < l2.val){
                 dummy.next = l1;
-                dummy = l1;
                 l1 = l1.next;
-            }
-            else{
+            }else{
                 dummy.next = l2;
-                dummy = l2;
                 l2 = l2.next;
             }
+            dummy = dummy.next;
         }
-        
-        return start.next;
+        dummy.next = (l1 == null) ? l2 : l1; 
+        return temp.next;
     }
 }
