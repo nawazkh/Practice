18a43ec5703797b4fbaabf6a248aa72267df10a7
Nawaz <k.nawaz.h@gmail.com>
2019-02-16 22:22:36 -0500

    recursive pascals triangle

--- JavaSolutions/PascalsTriangle.java
+++ JavaSolutions/PascalsTriangle.java
@@ -18,3 +18,24 @@ class Solution {
         return myList;
     }
 }
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+        helper(new ArrayList<Integer>(), numRows, triangle);
+        return triangle;
+    }
+    public void helper(List<Integer> lastRow, int numRows, List<List<Integer>> triangle){
+        int n = lastRow.size();
+        if(n == numRows) return;
+        List<Integer> temp = new ArrayList<Integer>();
+        for(int i = 0; i <=n; i++ ){
+            if(i == 0 || i == n) temp.add(1);
+            else{
+                temp.add(lastRow.get(i-1) + lastRow.get(i));
+            }
+        }
+        triangle.add(temp);
+        helper(temp,numRows,triangle);
+    }
+}
