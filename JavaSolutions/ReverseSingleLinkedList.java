93af9eb0c6cfcd6a047ee68e20d9c236f6b642df
Nawaz <k.nawaz.h@gmail.com>
2019-02-16 22:58:19 -0500

    recursive reverse a single linked list

--- JavaSolutions/ReverseSingleLinkedList.java
+++ JavaSolutions/ReverseSingleLinkedList.java
@@ -38,3 +38,22 @@ class Solution {
         return p.next;
     }
 }
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null) return head;
+        return helper(head);
+    }
+    public ListNode helper(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = null;
+        while(curr!= null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
