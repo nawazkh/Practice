52f7f17fc7bef8955fe96278111a0a40f9450a43
Nawaz <k.nawaz.h@gmail.com>
2019-02-19 15:29:33 -0500

    added O(logn) soln for rotating element

--- JavaSolutions/MinimuminRotatedArray.java
+++ JavaSolutions/MinimuminRotatedArray.java
@@ -1,8 +1,37 @@
 class Solution {
     public int findMin(int[] nums) {
-        for(int i = 0; i < nums.length - 1; i++){
-            if(nums[i + 1] < nums[i]) return nums[i+1];
+        int l = 0, h = nums.length - 1;
+        while(l < h){
+            int mid = l + (h - l)/2;
+            System.out.println(l+" "+mid+" "+h);
+            if(nums[h] > nums[mid]){// rotation in low to mid
+                h = mid;
+            }
+            else if(nums[mid] > nums[h]){
+                l = mid+1;
+            }else{
+                h--;// for repeated elements.
+            }
         }
-        return nums[nums.length - 1] < nums[0] ? nums[nums.length - 1] : nums[0];
+        return Math.min(nums[l],nums[h]);
     }
 }
+
+// for array with unique elements.
+class Solution {
+    public int findMin(int[] nums) {
+        int l = 0, h = nums.length - 1;
+        while(l < h){
+            int mid = l + (h - l)/2;
+            System.out.println(l+" "+mid+" "+h);
+            if(nums[h] > nums[mid]){// rotation in low to mid
+                h = mid;
+            }
+            else if(nums[mid] > nums[h]){
+                l = mid+1;
+            }
+        }
+        return nums[l];
+    }
+}
+
